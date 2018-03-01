3f3a6089b5e73dbb4f13cc071a09618319a57583
Aishwariya Bhavan <abhavan@ebsco.com>
2018-02-28 17:02:18 -0700

    add fraud-service impl

--- fraud-service/src/main/java/com/example/fraudservice/FraudServiceApplication.java
+++ fraud-service/src/main/java/com/example/fraudservice/FraudServiceApplication.java
@@ -2,11 +2,25 @@ package com.example.fraudservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class FraudServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FraudServiceApplication.class, args);
+	}
+}
+
+@RestController
+class FraudController {
+	@GetMapping("/frauds")
+	ResponseEntity<List<String>> frauds() {
+		return ResponseEntity.status(201).body(Arrays.asList("marcin", "josh"));
 	}
 }

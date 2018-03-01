7711c7e1b4f379b9814efb28e248bf75c9772947
Aishwariya Bhavan <abhavan@ebsco.com>
2018-02-28 18:23:49 -0700

    adding dependencies and baseclass

---
+++ fraud-service/src/test/java/com/example/fraudservice/BaseClass.java
@@ -0,0 +1,22 @@
+package com.example.fraudservice;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = FraudServiceApplication.class)
+@Ignore
+public class BaseClass {
+    @Autowired
+    FraudController fraudController;
+
+    @Before
+    public void setup() {
+        RestAssuredMockMvc.standaloneSetup(this.fraudController);
+    }
+}
